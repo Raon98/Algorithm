6baa1002046d980bebe40840dc2548c2771bae0d
Raon98 <cheol1998@kakao.com>
2023-02-09 15:59:38 +0900

    코테 공부

--- js/codeingTestHonnn/hoonnnStudy.js
+++ js/codeingTestHonnn/hoonnnStudy.js
@@ -1,6 +1,5 @@
 'use strict';
 
-//암호문 변역하기
 function solution() {
     const signal1 = {'a':'n','b ':'d','c':'a','d':'b','e':'e','f':'l','g':'j','h':'o','i':'z','j':'u','k':'y','l':'v','m':'w','n':'q','o':'x','p':'r','q':'p','r':'f','s':'g','t':'t','u':'m','v':'h','w':'i','x':'c','y':'k','z':'s'}
     const signal2 = {'a':'z','b ':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r','j':'q','k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'g','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a'}
@@ -10,28 +9,25 @@ function solution() {
 
     return [...binary].map((v,i)=> v==='0' ? signal1[cipher[i]]:signal2[cipher[i]]).join('')
 }
-
-/* 커피가격 맞추기 문제
+/*
 공부하면서 배운거
 1. reduce로 객체만들기
-    const drink = {'아메리카노' : 4100 , '카페라떼' : 4600, '카라멜마끼아또' : 5100}
-    => const drink = input.reduce((acc,cur,i) => {
-        return {...acc, [cur]: [4100,4200,5100][i]};
+    const food = input.reduce((acc,cur,i) => {
+        return {...acc, [cur]: [1000,2000,3000][i]};
     }, {});
-    으로 만들어서 주어지는 값이
+
 */
 function solution() {
-    let input = ['아메리카노','카페라떼','카라멜마끼아또']
-    //  const drink = {'아메리카노' : 4100 , '카페라떼' : 4600, '카라멜마끼아또' : 5100}
-    const drink = input.reduce((acc,cur,i) => {
-        return {...acc, [cur]: [4100,4200,5100][i]};
+    let input = ['솜사탕','물떡','떡볶이']
+    const food = input.reduce((acc,cur,i) => {
+        return {...acc, [cur]: [1000,2000,3000][i]};
     }, {});
 
     return input.reduce((acc,cur) => drink[cur]+acc,0)
 }
 
 
-// 앞뒤로 같은 문자일경우 찾기
+
 function solution() {
     var str = 'abcddaba'
     var result = ''
@@ -44,7 +40,7 @@ function solution() {
 
 
 
-//배열을 이용한방식
+
 function solution() {
     let num = 8
     let star = "*"
@@ -59,7 +55,7 @@ function solution() {
     }
     return arr.join('\n')
 }
-//이중 for문을 이용한 str 방식
+
 function solution() {
     let num = 8
     let star = "*"
@@ -76,7 +72,6 @@ function solution() {
     return str
 }
 
-//2,3,5,7을 포함한 배수찾기
 function solution(){
     let input = 114
     let arr = []
@@ -111,8 +106,6 @@ function solution() {
 
 
 
-//(a,b) = (4,7) 4부터 7까지 별찍기
-// b가 100이상일시 15개로 맥스고정
 function solution() {
     let a = 2
     let b = 100
@@ -130,8 +123,7 @@ function solution() {
     }
     return arr.join("\n");
 }
-//(a,b) = (4,7) 4부터 7까지 별찍기
-// b가 100이상일시 15개로 맥스고정
+
 function solution() {
     let a = 2
     let b = 100
